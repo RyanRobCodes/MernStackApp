a3aa7ae1079202a73b68bb9e94ddffdd3bedd3c8
Ryanrobcodes <ryan.robinsonlb@gmail.com>
2022-05-16 11:53:55 -0400

    push to heroku

--- server/server.js
+++ server/server.js
@@ -5,27 +5,39 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express')
 const { authMiddleware } = require('./utils/auth.js');
 const { RSA_NO_PADDING } = require('constants');
-const app = express();
 
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-const server = new ApolloServer({typeDefs,resolvers,context: authMiddleware,});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-server.applyMiddleware({ app });
-
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  RSA_NO_PADDING.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-//app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+  
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
